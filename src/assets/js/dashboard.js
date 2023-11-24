1f3e3364d1895e055787edd44856440146c0e66b
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-11-24 15:35:14 +0700

    updata

--- src/assets/js/dashboard.js
+++ src/assets/js/dashboard.js
@@ -1,113 +1,87 @@
 $(function () {
-
-
-  // =====================================
-  // Profit
-  // =====================================
-  var chart = {
-    series: [
-      { name: "Earnings this month:", data: [355, 390, 300, 350, 390, 180, 355, 390] },
-      { name: "Expense this month:", data: [280, 250, 325, 215, 250, 310, 280, 250] },
-    ],
-
-    chart: {
-      type: "bar",
-      height: 345,
-      offsetX: -15,
-      toolbar: { show: true },
-      foreColor: "#adb0bb",
-      fontFamily: 'inherit',
-      sparkline: { enabled: false },
-    },
-
-
-    colors: ["#5D87FF", "#49BEFF"],
-
-
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: "35%",
-        borderRadius: [6],
-        borderRadiusApplication: 'end',
-        borderRadiusWhenStacked: 'all'
-      },
-    },
-    markers: { size: 0 },
-
-    dataLabels: {
-      enabled: false,
-    },
-
-
-    legend: {
-      show: false,
-    },
-
-
-    grid: {
-      borderColor: "rgba(0,0,0,0.1)",
-      strokeDashArray: 3,
-      xaxis: {
-        lines: {
-          show: false,
+  // ดึงข้อมูล JSON จากไฟล์ PHP
+  function convertToThaiMonth(month) {
+    const monthNames = [
+      "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน",
+      "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"
+    ];
+  
+    const [year, monthNumber] = month.split("-");
+    const thaiMonth = monthNames[parseInt(monthNumber, 10) - 1];
+    return `${thaiMonth} ${parseInt(year, 10) + 543}`;
+  }
+  
+  $.ajax({
+    url: '../conf/condata.php', // แทนที่ชื่อไฟล์ตามที่คุณตั้งไว้
+    method: 'GET',
+    dataType: 'json',
+    success: function (jsonData) {
+      var profitChartOptions = {
+        series: [
+          { name: "121205", data: jsonData.map(item => parseFloat(item.total_amount_121205)) },
+          { name: "121206", data: jsonData.map(item => parseFloat(item.total_amount_121206)) },
+          { name: "121207", data: jsonData.map(item => parseFloat(item.total_amount_121207)) },
+          { name: "121208", data: jsonData.map(item => parseFloat(item.total_amount_121208)) }
+        ],
+        chart: {
+          type: "bar",
+          height: 345,
+          offsetX: -15,
+          toolbar: { show: true },
+          foreColor: "#adb0bb",
+          fontFamily: "inherit",
+          sparkline: { enabled: false },
         },
-      },
-    },
-
-    xaxis: {
-      type: "category",
-      categories: ["16/08", "17/08", "18/08", "19/08", "20/08", "21/08", "22/08", "23/08"],
-      labels: {
-        style: { cssClass: "grey--text lighten-2--text fill-color" },
-      },
-    },
-
-
-    yaxis: {
-      show: true,
-      min: 0,
-      max: 400,
-      tickAmount: 4,
-      labels: {
-        style: {
-          cssClass: "grey--text lighten-2--text fill-color",
-        },
-      },
-    },
-    stroke: {
-      show: true,
-      width: 3,
-      lineCap: "butt",
-      colors: ["transparent"],
-    },
-
-
-    tooltip: { theme: "light" },
-
-    responsive: [
-      {
-        breakpoint: 600,
-        options: {
-          plotOptions: {
-            bar: {
-              borderRadius: 3,
-            }
+        colors: ["#5D87FF", "#2A3547", "#13DEB9", "#FA896B"],
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: "35%",
+            borderRadius: [6],
+            borderRadiusApplication: "end",
+            borderRadiusWhenStacked: "all",
           },
-        }
-      }
-    ]
+        },
+        markers: { size: 0 },
+        dataLabels: { enabled: false },
+        legend: { show: false },
+        grid: {
+          borderColor: "rgba(0,0,0,0.1)",
+          strokeDashArray: 3,
+          xaxis: { lines: { show: false } },
+        },
+        xaxis: {
+          type: "category",
+          categories: jsonData.map(item => convertToThaiMonth(item.month)),
+          labels: { style: { cssClass: "grey--text lighten-2--text fill-color" } },
+        },
+        
+        yaxis: {
+          show: true,
+          min: 0,
+          tickAmount: 4,
+          labels: { style: { cssClass: "grey--text lighten-2--text fill-color" } },
+        },
+        stroke: { show: true, width: 3, lineCap: "butt", colors: ["transparent"] },
+        tooltip: { theme: "light" },
+        responsive: [
+          {
+            breakpoint: 600,
+            options: {
+              plotOptions: { bar: { borderRadius: 3 } },
+            },
+          },
+        ],
+      };
 
-
-  };
-
-  var chart = new ApexCharts(document.querySelector("#chart"), chart);
-  chart.render();
-
-
-  // =====================================
-  // Breakup
-  // =====================================
+      var profitChart = new ApexCharts(document.querySelector("#chart"), profitChartOptions);
+      profitChart.render();
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log('Error: ' + textStatus, errorThrown);
+    }
+    
+  });
   var breakup = {
     color: "#adb5bd",
     series: [38, 40, 25],
@@ -115,7 +89,7 @@ $(function () {
     chart: {
       width: 180,
       type: "donut",
-      fontFamily: "IBM Plex Sans Thai', sans-serif",
+      fontFamily: "Plus Jakarta Sans', sans-serif",
       foreColor: "#adb0bb",
     },
     plotOptions: {
@@ -158,54 +132,4 @@ $(function () {
 
   var chart = new ApexCharts(document.querySelector("#breakup"), breakup);
   chart.render();
-
-
-
-  // =====================================
-  // Earning
-  // =====================================
-  var earning = {
-    chart: {
-      id: "sparkline3",
-      type: "area",
-      height: 60,
-      sparkline: {
-        enabled: true,
-      },
-      group: "sparklines",
-      fontFamily: "IBM Plex Sans Thai', sans-serif",
-      foreColor: "#adb0bb",
-    },
-    series: [
-      {
-        name: "Earnings",
-        color: "#49BEFF",
-        data: [25, 66, 20, 40, 12, 58, 20],
-      },
-    ],
-    stroke: {
-      curve: "smooth",
-      width: 2,
-    },
-    fill: {
-      colors: ["#f3feff"],
-      type: "solid",
-      opacity: 0.05,
-    },
-
-    markers: {
-      size: 0,
-    },
-    tooltip: {
-      theme: "dark",
-      fixed: {
-        enabled: true,
-        position: "right",
-      },
-      x: {
-        show: false,
-      },
-    },
-  };
-  new ApexCharts(document.querySelector("#earning"), earning).render();
-})
+});
--- src/conf/footer.php
+++ src/conf/footer.php
@@ -1,3 +1,3 @@
 <div class="py-6 px-6 text-center">
-    <p class="mb-0 fs-4">Developed by </p>
+    <p class="mb-0 fs-4">Developed by phatcharapon.n@cmu.ac.th</p>
 </div>
