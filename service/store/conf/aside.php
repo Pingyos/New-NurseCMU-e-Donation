4917792cc2ad8c95d02901705251ff0666d6b99e
loveopo1999@gmail.com <loveopo1999@gmail.com>
2024-01-19 13:34:43 +0700

    updata

--- service/store/conf/aside.php
+++ service/store/conf/aside.php
@@ -38,7 +38,7 @@
                     <span class="hide-menu">ออกใบเสร็จ</span>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link" href="invoice_person.php" aria-expanded="false">
+                    <a class="sidebar-link" href="store_order.php" aria-expanded="false">
                         <span>
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-printer" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -47,33 +47,13 @@
                                 <path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" />
                             </svg>
                         </span>
-                        <span class="hide-menu">สำหรับ บุคคลทั่วไป</span>
+                        <span class="hide-menu">ใบจัดส่ง</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="invoice_corporation.php" aria-expanded="false">
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-printer" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" />
-                                <path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" />
-                                <path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" />
-                            </svg>
-                        </span>
-                        <span class="hide-menu">สำหรับ บริษัท/นิติบุคคล</span>
-                    </a>
-                </li>
+
                 <li class="nav-small-cap">
                     <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
                     <span class="hide-menu">ข้อมูลใบเสร็จ</span>
-                </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="invoice.php" aria-expanded="false">
-                        <span>
-                            <i class="ti ti-file-description"></i>
-                        </span>
-                        <span class="hide-menu">รายงานใบเสร็จ</span>
-                    </a>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link" href="invoice_report.php" aria-expanded="false">
