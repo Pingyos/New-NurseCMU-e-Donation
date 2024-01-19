df01f22a9ec4ceb8913d173f0a6011c1e636ba0a
loveopo1999@gmail.com <loveopo1999@gmail.com>
2024-01-19 13:14:56 +0700

    updata

--- service/store/conf/condata.php
+++ service/store/conf/condata.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "edonation";
+$password = "edonate@FON";
 $dbname = "edonation";
 
 try {
--- service/store/conf/header.php
+++ service/store/conf/header.php
@@ -23,9 +23,23 @@
                     </a>
                     <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
                         <div class="message-body">
-                            <a class="d-flex align-items-center gap-2 dropdown-item">
-                                <p class="mb-0 fs-3"><?php echo $loginInfo['prename_id'] . " " . $loginInfo['firstname_EN'] . " " . $loginInfo['lastname_EN'] . "<br>"; ?></p>
-                            </a>
+                            <div class="d-flex align-items-center py-9 mx-7 border-bottom">
+                                <?php
+                                $firstname_EN = $loginInfo['firstname_EN'];
+                                $imageFileName = "../assets/images/profile/" . $firstname_EN . ".jpg";
+                                if (file_exists($imageFileName)) {
+                                    echo '<img src="' . $imageFileName . '" alt width="100" height="100" class="rounded-circle">';
+                                } else {
+                                    echo '<img src="../assets/images/profile/default.jpg" alt width="100" height="100" class="rounded-circle">';
+                                }
+                                ?> <div class="ms-3">
+                                    <h5 class="mb-1 fs-3"><?php echo $loginInfo['prename_id'] . " " . $loginInfo['firstname_EN'] . " " . $loginInfo['lastname_EN'] . "<br>"; ?></h5>
+                                    <span class="mb-1 d-block"><?php echo $loginInfo['organization_name_EN']; ?></span>
+                                    <p class="mb-0 d-flex align-items-center gap-2">
+                                        <i class="ti ti-mail fs-4"></i> <?php echo $loginInfo['cmuitaccount']; ?>
+                                    </p>
+                                </div>
+                            </div>
                             <a href="user_view.php" class="d-flex align-items-center gap-2 dropdown-item">
                                 <i class="ti ti-user fs-6"></i>
                                 <p class="mb-0 fs-3">ข้อมูลส่วนตัว</p>
